2bfd5c3802f40cd588113b54622685b19f68fad2
cbandarac58-del <cbandarac58@gmail.com>
2026-09-20 10:01:37 +0530

    Create sitemap-config.js

---
+++ src/utils/sitemap-config.js
@@ -0,0 +1,5 @@
+// How many sitemap files to publish. Increase these slowly over time.
+// Each movie/TV file covers 20 TMDB pages, roughly 400 titles.
+export const MOVIE_SITEMAPS = 2;
+export const TV_SITEMAPS = 1;
+export const PAGES_PER_SITEMAP = 20;
