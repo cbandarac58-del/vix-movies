55093a2924bff2bcd2e391aa84245d74f650df94
cbandarac58-del <cbandarac58@gmail.com>
2026-09-26 09:13:05 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,21 @@
 import { defineMiddleware } from 'astro:middleware';
+import { getSiteAdsScript } from './utils/site-ads.js';
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const response = await next();
+
   const type = response.headers.get('content-type') || '';
-  if (!type.includes('text/html')) return response;
+
+  // HTML pages only
+  if (!type.includes('text/html')) {
+    return response;
+  }
 
   let html = await response.text();
+
+  /*
+   * Favicon
+   */
   if (!html.includes('rel="icon"')) {
     html = html.replace(
       '</head>',
@@ -13,8 +23,24 @@ export const onRequest = defineMiddleware(async (context, next) => {
     );
   }
 
+  /*
+   * Global Vix Movies Ads
+   *
+   * This is injected into every HTML page automatically.
+   * New .astro pages will also receive ads.
+   */
+  if (!html.includes('__VIX_GLOBAL_ADS_LOADED')) {
+    html = html.replace(
+      '</body>',
+      getSiteAdsScript() + '</body>'
+    );
+  }
+
   const headers = new Headers(response.headers);
+
+  // Body size changed after HTML injection
   headers.delete('content-length');
+
   return new Response(html, {
     status: response.status,
     statusText: response.statusText,
